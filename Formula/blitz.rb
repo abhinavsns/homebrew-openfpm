1b2783397eea13b84392c4c999db208c84de4657
absingh <absingh@mpi-cbg.de>
2024-05-18 13:45:42 +0200

    update sha256

--- Formula/blitz.rb
+++ Formula/blitz.rb
@@ -2,6 +2,7 @@ class Blitz < Formula
   desc "Blitz"
   homepage "https://github.com/blitzpp/blitz"
   url "http://ppmcore.mpi-cbg.de/upload/blitz-1.0.2.tar.gz"
+  sha256 "500db9c3b2617e1f03d0e548977aec10d36811ba1c43bb5ef250c0e3853ae1c2"
   version "1.0.2"
 
   depends_on "cmake" => :build
--- Formula/libhilbert.rb
+++ Formula/libhilbert.rb
@@ -2,7 +2,8 @@ class Libhilbert < Formula
   desc "Library producing Hilbert indices for multidimensional data"
   homepage "https://github.com/pdebuyl/libhilbert"
   url "http://ppmcore.mpi-cbg.de/upload/libhilbert-master.tar.gz"
-  version "1.0.0" # Replace with the actual version
+  sha256 "546d5dec403d7ed1e71706ee39e10d69496a6753eb7f116afe1089d85dc9c35a"
+  version "1.0.0"
   revision 1
 
   # Add any dependencies here, for example:
