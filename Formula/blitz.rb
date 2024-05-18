1d3a4751a57a10ac111f33906542d68d4febeddc
absingh <absingh@mpi-cbg.de>
2024-05-18 16:58:35 +0200

    update and fix

--- Formula/blitz.rb
+++ Formula/blitz.rb
@@ -1,5 +1,5 @@
 class Blitz < Formula
-  desc "Levelset utility"
+  desc "Array utility"
   homepage "https://github.com/blitzpp/blitz"
   url "http://ppmcore.mpi-cbg.de/upload/blitz-1.0.2.tar.gz"
   sha256 "500db9c3b2617e1f03d0e548977aec10d36811ba1c43bb5ef250c0e3853ae1c2"
@@ -9,10 +9,10 @@ class Blitz < Formula
 
   def install
     # Create and move to the build directory
-    build_dir = "build"
-    mkdir build_dir do
+    mkdir_p "build"
+    cd "build" do
       # Run cmake and make from within the build directory
-      system "cmake", "..", *std_cmake_args, "-DBUILD_TESTING=OFF"
+      system "cmake", "..", "-DBUILD_TESTING=OFF"
       system "make", "install"
     end
   end
